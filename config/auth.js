b0b44b6eeaed783a31b828d5d8de4ddd021b4447
Anusha222 <anushamekala35@gmail.com>
2017-04-25 11:49:08 +0530

    Signed-off-by: Anusha222 <anushamekala35@gmail.com>

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '402927050081692',
+		'clientSecret': '96d63f80260351441be4f3029fba1334',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': '464027654576-d5e75ac55b3e7mq66krm8qjo1aeni7oj.apps.googleusercontent.com',
+		'clientSecret': 'tvob5H2j2qxIvNqPRXI9QyWM',
+		'callbackURL': 'http://localhost:8080/auth/google/callback'
+	}
+}
